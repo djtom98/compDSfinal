57b248185b1c70479b7d2f36f14019e0ab80e193
Lucas Costa <117675445+lucascs0304@users.noreply.github.com>
2022-12-20 17:29:48 +0100

    some changes in the functions

--- build/lib/learnlibs/read_data.py
+++ build/lib/learnlibs/read_data.py
@@ -1,14 +1,49 @@
-import pandas as pd
+# Part A
 
-def read_data(path: str):
-    '''
-    Read database.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+
+class loader():
     
-    Parameters: 
-        path: path to data
+    def __init__(self, file):
+        self.file = file
+        self.load()
+        self.split()
         
-    Returns:
-        df (dataframe): database in pandas format
-    '''
-    df = pd.read_csv(path, index_col=0)
-    return df
+    def load(self):
+        
+        '''
+        Read database.
+        
+        Parameters: 
+            file: path to data
+            
+        Returns:
+            df: database in pandas format
+        '''
+        
+        self.df = pd.read_csv(self.file, index_col=0)
+        return self.df
+      
+
+    def split(self):
+
+        
+        '''
+        Split data between train and test using sklearn.model_selection.train_test_split.
+        
+        Parameters: 
+            df: dataframe to split data for analysis 
+            
+        Returns:
+            train: splitted dataframe for training models
+            test: splitted dataframe for training models
+            
+        '''
+        
+        seed = 2
+        self.train, self.test = train_test_split(self.df, test_size=0.3, random_state=seed)
+
+        return self.train, self.test
