4ce6699d49ae884496a2bc600ea1a67e101dff2c
djtom <davisjacobthomas@gmail.com>
2022-12-21 00:45:22 +0100

    removed unnecessary files using gitignore

--- build/lib/learnlibs/metrics.py
+++
@@ -1,9 +0,0 @@
-    def get_roc_auc_score(self):
-        '''
-        Obtains the roc_auc metric for  test sets.
-            
-        '''
-        # obtain scores
-        test_auc = roc_auc_score(self.y_test, self.test_proba)
-        
-        return test_auc    
